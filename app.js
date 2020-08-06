48217c9a7492e0632511c0b291a36d8c833ec5ae
the_nerd <devilrhook.dr@gmail.com>
2020-08-06 16:03:58 +0000

    add database url to environment variable

--- app.js
+++ app.js
@@ -18,18 +18,13 @@ var express 		= require('express'),
 var campgroundsRoutes = require("./routes/campgrounds"),
 	commentsRoutes	  = require("./routes/comments"),
 	indexRoutes		  = require("./routes/index");
-	// campgrounds = [
-	// 	{name:'Salmon Greek', image:'https://th.bing.com/th/id/OIP.8fVlBBHp73Neid3I9Aj82AHaFj?w=212&h=180&c=7&o=5&dpr=1.25&pid=1.7'},
-	// 	{name:'Granite Hill', image:'https://th.bing.com/th/id/OIP.iezcBRbEEDRNQHDu0ibozQHaDb?w=308&h=162&c=7&o=5&dpr=1.25&pid=1.7'},
-	// 	{name:"Mountain goat's rest", image:'https://th.bing.com/th/id/OIP.ifjgRwGw_0cKbBU5im0bXQHaEK?w=316&h=180&c=7&o=5&dpr=1.25&pid=1.7'}
-	// ]
-
-mongoose.connect("mongodb+srv://the_nerd:"+process.env.MONGO_PASS+"@cluster0.oebvw.mongodb.net/yelpcamp_deployed?retryWrites=true&w=majority", {
-	useNewUrlParser: true,
+	
+mongoose.connect(process.env.DB_URL, {
+	newUrlParser: true,
 	useUnifiedTopology: true,
-	useFindAndModify: false
+	useFindAndModify: true
 })
-.then(()=>console.log("Connected to DB"))
+.then(()=>console.log("connected to DB"))
 .catch((err)=>console.log(err));
 
 // seedDB();  //seed the database;
