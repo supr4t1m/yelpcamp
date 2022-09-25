a99f76ef6cca00e84eac86c0efa2588ff7555b01
the_nerd <devilrhook.dr@gmail.com>
2022-09-25 04:05:25 +0000

    Added profile page
    The profile page of a user can be viewed from the campgrounds show page
    or from the navbar when the user is logged in. The profile is public by
    default. Logic for private profile will be added later.

--- models/user.js
+++ models/user.js
@@ -4,6 +4,11 @@ var mongoose 				= require("mongoose"),
 var userSchema = new mongoose.Schema({
 	username: String,
 	password: String,
+	firstName: String,
+	lastName: String,
+	email: String, 
+	avatar: String,
+	bio: String
 });
 
 userSchema.plugin(passportLocalMongoose);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require("express"),
 	router  = express.Router(),
 	passport = require("passport"),
-	User    = require("../models/user");
+	User    = require("../models/user"),
+	Campground = require("../models/campgrounds");
 
 //ROOT route
 
@@ -17,7 +18,15 @@ router.get("/register", function(req, res) {
 });
 
 router.post("/register", function(req, res) {
-	var newUser = new User({username: req.body.username});
+	// do not store password as password will be hashed by passport js before getting stored
+	var newUser = new User({
+		username: req.body.username,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		avatar: req.body.avatar,
+		bio: req.body.bio
+	});
 	User.register(newUser, req.body.password, function(err, user){
 		if(err) {
 			return res.render("register", {error: err.message});
@@ -62,5 +71,36 @@ function isLoggedIn(req, res, next) {
 	}
 	res.redirect("/login");
 }
+
+// USER PROFILE
+router.get("/users/:id", function(req, res) {
+	User.findById(req.params.id, function(err, user) {
+		const backUrl = req.headers.referer || "/campgrounds";
+		
+		if (err) {
+			req.flash("error", "error finding user");
+			return res.redirect(backUrl);
+		}
+		
+		if (!user) {
+			req.flash("error", "user doesn't exist");
+			return res.redirect(backUrl);
+		}
+		
+		// we have built up the query before executing, this way we don't have to pass json objects as query
+		
+		// .id is a virtual getter function to return string of ._id, set default by mongoose
+		Campground.find().where("author.id").equals(user.id).exec(function(err, campgrounds) {
+			// campgrounds object is passed without a key, the key will automatically be named campgrounds with values as contents of campgrounds object
+			// same with user
+			if (err) {
+				req.flash("error", "unable to find campgrounds with current user, something went wrong.");
+				return res.redirect(backUrl);
+			}
+			
+			res.render("users/shows", {user, campgrounds});
+		})
+	})
+})
 
 module.exports = router;
